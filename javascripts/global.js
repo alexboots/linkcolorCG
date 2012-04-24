61935c48ddd957c6604cb71ceee4ba0b5651dfd1
Alex Hays <alexhays@gmail.com>
2012-04-24 13:45:23 -0500

    first commit

---
+++ javascripts/global.js
@@ -0,0 +1,34 @@
+$(function(){
+  
+  $(".coupon a").click(function(event){
+      event.preventDefault();
+      window.open("show_code.html", "coupons", "width=499, height=256");
+  });
+
+  if(!Modernizr.input.placeholder){
+
+    $('[placeholder]').focus(function() {
+      var input = $(this);
+      if (input.val() == input.attr('placeholder')) {
+      input.val('');
+      input.removeClass('placeholder');
+      }
+    }).blur(function() {
+      var input = $(this);
+      if (input.val() == '' || input.val() == input.attr('placeholder')) {
+      input.addClass('placeholder');
+      input.val(input.attr('placeholder'));
+      }
+    }).blur();
+    $('[placeholder]').parents('form').submit(function() {
+      $(this).find('[placeholder]').each(function() {
+      var input = $(this);
+      if (input.val() == input.attr('placeholder')) {
+        input.val('');
+      }
+      })
+    });
+
+  }
+
+});
